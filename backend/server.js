feafec2eb1f41968ceb78d8e1744b6a405936822
priya <impriya840@gmail.com>
2023-07-11 21:45:48 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -25,11 +25,11 @@ app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
 
 // if(process.env.NODE_ENV==='production'){
-// app.use(express.static(path.join(__dirname, './frontend/build')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, './frontend/build/index.html'));
-// });
-// }
+app.use(express.static(path.join(__dirname, './frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+});
+
 // else{
 //  app.get('/',(req,res)=>res.send(' please set to production'))
 // }
